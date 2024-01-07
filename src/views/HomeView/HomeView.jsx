2c190d9c01c8bd36c606c0585fd17f418b01732e
Nicovied0 <briannoviedo@gmail.com>
2024-01-07 01:01:09 -0300

    first commit

---
+++ src/views/HomeView/HomeView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const HomeView = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello, CSS Modules!</h1>
+    </div>
+  );
+};
+
+export default HomeView;
